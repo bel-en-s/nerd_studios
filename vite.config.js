f21aac1d6db96e79b4a0f8f7fd75b2cc44911a65
belen-novaworks <belen.seoane.palmieri@gmail.com>
2026-07-30 14:09:44 -0300

    vite

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,17 @@
-import { defineConfig } from "vite"
-import { resolve } from "path"
+import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
-	plugins: [],
-	build: {
-		rollupOptions: {
-			input: {
-				main: resolve(__dirname, "index.html"),
-				gallery: resolve(__dirname, "gallery.html"),
-				shop: resolve(__dirname, "shop.html"),
-				product: resolve(__dirname, "product.html"),
-			},
-		},
-	},
-})
+  base: "/nerd_studios/",
+
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        gallery: resolve(__dirname, "gallery.html"),
+        shop: resolve(__dirname, "shop.html"),
+        product: resolve(__dirname, "product.html"),
+      },
+    },
+  },
+});
